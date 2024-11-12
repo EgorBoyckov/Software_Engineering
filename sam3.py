169e4430e072eae62d71c566a1b02169bf103329
EgorBoyckov <boyckow.egor@mail.ru>
2024-11-13 00:02:34 +0500

    FINALv1

--- sam3.py
+++ sam3.py
@@ -1,9 +1,8 @@
 def sumNumber(a,b):
     try:
         return a + b
-    except TypeError as e:
-        print(e)
-        print("Неподходящий тип данных. Ожидалось число")
+    except TypeError:
+        return ("Неподходящий тип данных. Ожидалось число")
 if __name__ == "__main__":
-    sumNumber(2,"2")
-    sumNumber(21,19)
+    print(sumNumber(2,"2"))
+    print(sumNumber(21,19))
